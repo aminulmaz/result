bd4ebde3b8168977a07c626da579b25ba07b4a3e
Dancing bull <72798082+upscaspirantmzr@users.noreply.github.com>
2025-09-27 14:50:35 +0530

    Create firebaseconfig.js

---
+++ firebaseconfig.js
@@ -0,0 +1,14 @@
+
+const firebaseConfig = {
+  const firebaseConfig = {
+    apiKey: "AIzaSyDnxjj8r-FsGpKw8mJXyrRMrzm_Z050Rgg",
+    authDomain: "resultmzr.firebaseapp.com",
+    projectId: "resultmzr",
+    storageBucket: "resultmzr.firebasestorage.app",
+    messagingSenderId: "825199653263",
+    appId: "1:825199653263:web:a1fdc3e15724f9d4609bfa",
+    measurementId: "G-JGVR6TDH27"
+  };
+
+// You can keep this part as is.
+export { firebaseConfig };
